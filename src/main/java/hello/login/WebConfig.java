c46b1c68785bf69ae7c0f879f7ca1c38b6bd8b1a
dohyun97 <dohyun647@gmail.com>
2023-04-14 14:18:18 -0400

    After login, redirect to last request URI

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,15 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter()); //등록할 필터 지정
         filterFilterRegistrationBean.setOrder(1); //필터는 체인으로 동작해서 순서 필요. 낮을 수록 먼저 동작
+        filterFilterRegistrationBean.addUrlPatterns("/*"); //필터를 적용할 URL 패턴. 한번에 여러 패턴 지정 가능
+        return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter()); //등록할 필터 지정
+        filterFilterRegistrationBean.setOrder(2); //필터는 체인으로 동작해서 순서 필요. 낮을 수록 먼저 동작
         filterFilterRegistrationBean.addUrlPatterns("/*"); //필터를 적용할 URL 패턴. 한번에 여러 패턴 지정 가능
         return filterFilterRegistrationBean;
     }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -73,7 +74,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult , HttpServletRequest request){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -96,6 +97,33 @@ public class LoginController {
 
 
         return "redirect:/";
+    }
+
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute LoginForm form, @RequestParam(defaultValue = "/") String redirectURL,
+                          BindingResult bindingResult , HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}",loginMember);
+        //fail login
+        if(loginMember == null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        //로그인 성공시
+
+        //세션 있으면 있는 세션 반환,없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+
+        return "redirect:"+redirectURL;
     }
 
     //@PostMapping("/logout")
