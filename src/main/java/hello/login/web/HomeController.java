d9e21a91cb2064ee6049b43f66c42d789fc983f6
dohyun97 <dohyun647@gmail.com>
2023-04-13 20:48:58 -0400

    cookie login and logout

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,5 +1,8 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
-
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+    //@GetMapping("/")
     public String home() {
         return "home";
     }
 
-
+    @GetMapping("/")
+    public String home(@CookieValue(name = "memberId",required = false) Long memberId,Model model){
+        //쿠키 없을 때. 로그인 안했을때
+        if(memberId == null){
+            return "home";
+        }
+        Member loginMember = memberRepository.findById(memberId);
+        //쿠키는 있으나 db에 없는 멤버
+        if(loginMember == null){
+            return "home";
+        }
+        //로그인한 멤버
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
 }
